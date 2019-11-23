c650646fd20aaeae3d2380af942fd79b07737ecf
DCOFFEY20 <dcoffey20@gmail.com>
2019-11-23 10:57:24 -0600

    created html file and javascript to get current weather at searched location

---
+++ script.js
@@ -0,0 +1,27 @@
+let apiKey = "0b4ce2adf9660ea726ae320d56010a24";
+let city;
+let weather;
+
+// console.log(weather);
+
+$("#citySearchButton").click(function () {
+    event.preventDefault();
+    city = $("#citySearch").val();
+    // console.log(city);
+    let weather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+
+    $.ajax({
+        url: weather,
+        method: "GET"
+    }).then(function (response) {
+        $("#currentWeather").text(response.name);
+        $("#currentDate").text(moment().format("(L)"));
+        // $("#currentIcon").
+        let tempF = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+        $("#currentTemp").text("Temperature: " + tempF + "°F");
+        $("#currentHumid").text("Humidity: " + response.main.humidity + "%");
+        $("#currentWind").text("Wind Speed: " + response.wind.speed + "MPH");
+        console.log(response);
+    });
+});
+
