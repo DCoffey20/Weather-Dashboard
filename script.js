26cdf50cd922d2d32b48087343d8def049039a69
DCOFFEY20 <dcoffey20@gmail.com>
2019-11-25 16:05:07 -0600

    added 5 day forecast

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let apiKey = "0b4ce2adf9660ea726ae320d56010a24";
 let city;
 let weather;
+let forecast;
 
 // console.log(weather);
 
@@ -9,6 +10,8 @@ $("#citySearchButton").click(function () {
     city = $("#citySearch").val();
     // console.log(city);
     let weather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+
+    $("#cityList").append(city);
 
     $.ajax({
         url: weather,
@@ -21,6 +24,45 @@ $("#citySearchButton").click(function () {
         $("#currentTemp").text("Temperature: " + tempF + "°F");
         $("#currentHumid").text("Humidity: " + response.main.humidity + "%");
         $("#currentWind").text("Wind Speed: " + response.wind.speed + "MPH");
+        // console.log(response);
+    });
+
+    let forecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=" + apiKey;
+
+    $.ajax({
+        url: forecast,
+        method: "GET"
+    }).then(function (response){
+        // Day 1
+        $("#day1").text(moment().add(1,"d").format("(L)"));
+        $("#day1Icon").attr("src", "http://openweathermap.org/img/w/" + response.list[5].weather[0].icon + ".png");
+        let day1TempF = Math.round((response.list[5].main.temp - 273.15) * 1.80 + 32);
+        $("#day1Temp").text("Temp: " + day1TempF + "°F");
+        $("#day1Humid").text("Humidity: " + response.list[5].main.humidity + "%");
+        // Day 2
+        $("#day2").text(moment().add(2,"d").format("(L)"));
+        $("#day2Icon").attr("src", "http://openweathermap.org/img/w/" + response.list[13].weather[0].icon + ".png");
+        let day2TempF = Math.round((response.list[13].main.temp - 273.15) * 1.80 + 32);
+        $("#day2Temp").text("Temp: " + day2TempF + "°F");
+        $("#day2Humid").text("Humidity: " + response.list[13].main.humidity + "%");
+        // Day 3
+        $("#day3").text(moment().add(3,"d").format("(L)"));
+        $("#day3Icon").attr("src", "http://openweathermap.org/img/w/" + response.list[21].weather[0].icon + ".png");
+        let day3TempF = Math.round((response.list[21].main.temp - 273.15) * 1.80 + 32);
+        $("#day3Temp").text("Temp: " + day3TempF + "°F");
+        $("#day3Humid").text("Humidity: " + response.list[21].main.humidity + "%");
+        // Day 4
+        $("#day4").text(moment().add(4,"d").format("(L)"));
+        $("#day4Icon").attr("src", "http://openweathermap.org/img/w/" + response.list[29].weather[0].icon + ".png");
+        let day4TempF = Math.round((response.list[29].main.temp - 273.15) * 1.80 + 32);
+        $("#day4Temp").text("Temp: " + day4TempF + "°F");
+        $("#day4Humid").text("Humidity: " + response.list[29].main.humidity + "%");
+        // Day 5
+        $("#day5").text(moment().add(5,"d").format("(L)"));
+        $("#day5Icon").attr("src", "http://openweathermap.org/img/w/" + response.list[37].weather[0].icon + ".png");
+        let day5TempF = Math.round((response.list[37].main.temp - 273.15) * 1.80 + 32);
+        $("#day5Temp").text("Temp: " + day5TempF + "°F");
+        $("#day5Humid").text("Humidity: " + response.list[37].main.humidity + "%");
         console.log(response);
     });
 });
